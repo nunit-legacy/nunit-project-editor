8fd415dfffd435ed48e6262a6737d70972c38935
Charlie Poole <charlie@nunit.org>
2011-01-27 20:10:04 -0800

    Split ConfigurationEditorView and RenameConfigurationDialog into partial files

--- src/editor/ConfigurationEditor/ConfigurationEditorView.cs
+++ src/editor/ConfigurationEditor/ConfigurationEditorView.cs
@@ -35,25 +35,9 @@ namespace NUnit.ProjectEditor
 	/// ConfigurationEditor form is designed for adding, deleting
 	/// and renaming configurations from a doc.
 	/// </summary>
-	public class ConfigurationEditorView : System.Windows.Forms.Form, IConfigurationEditorView
+	public partial class ConfigurationEditorView : System.Windows.Forms.Form, IConfigurationEditorView
     {
-        #region Instance Variables
-
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
-		private System.Windows.Forms.ListBox configListBox;
-		private System.Windows.Forms.Button removeButton;
-		private System.Windows.Forms.Button renameButton;
-		private System.Windows.Forms.Button addButton;
-		private System.Windows.Forms.Button activeButton;
-		private System.Windows.Forms.HelpProvider helpProvider1;
-		private System.Windows.Forms.Button closeButton;
-
-        #endregion
-
-		#region Construction and Disposal
+		#region Constructor
 
 		public ConfigurationEditorView()
 		{
@@ -65,21 +49,6 @@ namespace NUnit.ProjectEditor
             ActiveCommand = new ButtonElement(activeButton);
 
             ConfigList = new ListControlWrapper(configListBox);
-		}
-
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
 		}
 
 		#endregion
@@ -144,110 +113,5 @@ namespace NUnit.ProjectEditor
         #endregion
 
         #endregion
-
-        #region Windows Form Designer generated code
-        /// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(ConfigurationEditorView));
-			this.configListBox = new System.Windows.Forms.ListBox();
-			this.removeButton = new System.Windows.Forms.Button();
-			this.renameButton = new System.Windows.Forms.Button();
-			this.closeButton = new System.Windows.Forms.Button();
-			this.addButton = new System.Windows.Forms.Button();
-			this.activeButton = new System.Windows.Forms.Button();
-			this.helpProvider1 = new System.Windows.Forms.HelpProvider();
-			this.SuspendLayout();
-			// 
-			// configListBox
-			// 
-			this.helpProvider1.SetHelpString(this.configListBox, "Selects the configuration to operate on.");
-			this.configListBox.ItemHeight = 16;
-			this.configListBox.Location = new System.Drawing.Point(8, 8);
-			this.configListBox.Name = "configListBox";
-			this.helpProvider1.SetShowHelp(this.configListBox, true);
-			this.configListBox.Size = new System.Drawing.Size(168, 212);
-			this.configListBox.TabIndex = 0;
-			// 
-			// removeButton
-			// 
-			this.helpProvider1.SetHelpString(this.removeButton, "Removes the selected configuration");
-			this.removeButton.Location = new System.Drawing.Point(192, 8);
-			this.removeButton.Name = "removeButton";
-			this.helpProvider1.SetShowHelp(this.removeButton, true);
-			this.removeButton.Size = new System.Drawing.Size(96, 32);
-			this.removeButton.TabIndex = 1;
-			this.removeButton.Text = "&Remove";
-			// 
-			// renameButton
-			// 
-			this.helpProvider1.SetHelpString(this.renameButton, "Allows renaming the selected configuration");
-			this.renameButton.Location = new System.Drawing.Point(192, 48);
-			this.renameButton.Name = "renameButton";
-			this.helpProvider1.SetShowHelp(this.renameButton, true);
-			this.renameButton.Size = new System.Drawing.Size(96, 32);
-			this.renameButton.TabIndex = 2;
-			this.renameButton.Text = "Re&name...";
-			// 
-			// closeButton
-			// 
-			this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.helpProvider1.SetHelpString(this.closeButton, "Closes this dialog");
-			this.closeButton.Location = new System.Drawing.Point(192, 216);
-			this.closeButton.Name = "closeButton";
-			this.helpProvider1.SetShowHelp(this.closeButton, true);
-			this.closeButton.Size = new System.Drawing.Size(96, 32);
-			this.closeButton.TabIndex = 4;
-			this.closeButton.Text = "Close";
-			// 
-			// addButton
-			// 
-			this.helpProvider1.SetHelpString(this.addButton, "Allows adding a new configuration");
-			this.addButton.Location = new System.Drawing.Point(192, 88);
-			this.addButton.Name = "addButton";
-			this.helpProvider1.SetShowHelp(this.addButton, true);
-			this.addButton.Size = new System.Drawing.Size(96, 32);
-			this.addButton.TabIndex = 5;
-			this.addButton.Text = "&Add...";
-			// 
-			// activeButton
-			// 
-			this.helpProvider1.SetHelpString(this.activeButton, "Makes the selected configuration active");
-			this.activeButton.Location = new System.Drawing.Point(192, 128);
-			this.activeButton.Name = "activeButton";
-			this.helpProvider1.SetShowHelp(this.activeButton, true);
-			this.activeButton.Size = new System.Drawing.Size(96, 32);
-			this.activeButton.TabIndex = 6;
-			this.activeButton.Text = "&Make Active";
-			// 
-			// ConfigurationEditor
-			// 
-			this.AcceptButton = this.closeButton;
-			this.CancelButton = this.closeButton;
-			this.ClientSize = new System.Drawing.Size(297, 267);
-			this.ControlBox = false;
-			this.Controls.Add(this.activeButton);
-			this.Controls.Add(this.addButton);
-			this.Controls.Add(this.closeButton);
-			this.Controls.Add(this.renameButton);
-			this.Controls.Add(this.removeButton);
-			this.Controls.Add(this.configListBox);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.HelpButton = true;
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "ConfigurationEditor";
-			this.helpProvider1.SetShowHelp(this, false);
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Configuration Editor";
-			this.ResumeLayout(false);
-
-		}
-		#endregion
     }
 }
--- src/editor/ConfigurationEditor/RenameConfigurationDialog.cs
+++ src/editor/ConfigurationEditor/RenameConfigurationDialog.cs
@@ -35,7 +35,7 @@ namespace NUnit.ProjectEditor
     /// validates the name. The caller is responsible for
     /// actually renaming the cofiguration.
     /// </summary>
-	public class RenameConfigurationDialog : System.Windows.Forms.Form
+	public partial class RenameConfigurationDialog : System.Windows.Forms.Form
 	{
 		#region Instance Variables
 
@@ -54,18 +54,9 @@ namespace NUnit.ProjectEditor
         /// </summary>
         private string[] configList;
 
-		private System.Windows.Forms.Button okButton;
-		private System.Windows.Forms.Button cancelButton;
-		private System.Windows.Forms.TextBox configurationNameTextBox;
-
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
-
 		#endregion
 
-		#region Construction and Disposal
+		#region Constructor
 
 		public RenameConfigurationDialog( string configurationName, string[] configList )
 		{
@@ -75,79 +66,6 @@ namespace NUnit.ProjectEditor
             this.configList = configList;
         }
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
-
-		#endregion
-
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.configurationNameTextBox = new System.Windows.Forms.TextBox();
-			this.okButton = new System.Windows.Forms.Button();
-			this.cancelButton = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// configurationNameTextBox
-			// 
-			this.configurationNameTextBox.Location = new System.Drawing.Point(16, 16);
-			this.configurationNameTextBox.Name = "configurationNameTextBox";
-			this.configurationNameTextBox.Size = new System.Drawing.Size(264, 22);
-			this.configurationNameTextBox.TabIndex = 0;
-			this.configurationNameTextBox.Text = "";
-			this.configurationNameTextBox.TextChanged += new System.EventHandler(this.configurationNameTextBox_TextChanged);
-			// 
-			// okButton
-			// 
-			this.okButton.Location = new System.Drawing.Point(56, 48);
-			this.okButton.Name = "okButton";
-			this.okButton.Size = new System.Drawing.Size(75, 24);
-			this.okButton.TabIndex = 1;
-			this.okButton.Text = "OK";
-			this.okButton.Click += new System.EventHandler(this.okButton_Click);
-			// 
-			// cancelButton
-			// 
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.Location = new System.Drawing.Point(160, 48);
-			this.cancelButton.Name = "cancelButton";
-			this.cancelButton.Size = new System.Drawing.Size(75, 24);
-			this.cancelButton.TabIndex = 2;
-			this.cancelButton.Text = "Cancel";
-			// 
-			// RenameConfigurationDialog
-			// 
-			this.AcceptButton = this.okButton;
-			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(291, 79);
-			this.Controls.Add(this.cancelButton);
-			this.Controls.Add(this.okButton);
-			this.Controls.Add(this.configurationNameTextBox);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-			this.Name = "RenameConfigurationDialog";
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Rename Configuration";
-			this.Load += new System.EventHandler(this.ConfigurationNameDialog_Load);
-			this.ResumeLayout(false);
-
-		}
 		#endregion
 
 		#region Properties & Methods
